b0d153d719cf81c6d9701061e9f703792c582700
Saiyam Pathak <saiyam@civo.com>
2022-03-01 15:26:45 +0530

    k8s+CRIO

---
+++ kubecrio/foreground.sh
@@ -0,0 +1,1 @@
+curl https://gist.githubusercontent.com/saiyam1814/0895423eb988a54a72998f40a3fc36da/raw/fd257f9c3983ff5aec2902f2fc1884b56d5fe359/Kubernetes%25201.23%2520+%2520CRIO%2520setup | sh
---
+++ kubecrio/intro.md
@@ -0,0 +1,1 @@
+This is a single node Kubernetes1.23+CRIO cluster that can help you play around with Kubernetes 1.23.
