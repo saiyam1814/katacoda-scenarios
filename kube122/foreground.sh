a4ae966ac357d2258b9d422b738137b142112205
Saiyam Pathak <saiyam@civo.com>
2022-04-10 22:51:47 +0530

    Update foreground.sh

--- kube122/foreground.sh
+++ kube122/foreground.sh
@@ -1,1 +1,1 @@
-curl https://gist.githubusercontent.com/saiyam1814/531ddc3b33ddc05b7a305e26756492f2/raw/3f00ff06c9b119c668f71fd0bbf0d1de872e4ac8/install.sh | sh 
+curl https://gist.githubusercontent.com/saiyam1814/531ddc3b33ddc05b7a305e26756492f2/raw/e34bf9a2ddee499ccdab1acabc9207d61e8466ca/install.sh | sh 
