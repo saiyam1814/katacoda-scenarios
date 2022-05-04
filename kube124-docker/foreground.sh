87d920a6573179f17bd6cdfc8af43b4f177a6422
Saiyam Pathak <saiyam@civo.com>
2022-05-04 18:02:14 +0530

    kubernetes1.24+docker

---
+++ kube124-docker/foreground.sh
@@ -0,0 +1,1 @@
+curl https://gist.githubusercontent.com/saiyam1814/801db1288c690a969e7174eca89c65b2/raw/944bb79e3f08da923145ed5ffc76b3b88df491ed/kube124-docker.sh | sh 
---
+++ kube124-docker/intro.md
@@ -0,0 +1,1 @@
+This is a single node Kubernetes1.24+dockerd cluster that can help you play around with Kubernetes 1.24.
