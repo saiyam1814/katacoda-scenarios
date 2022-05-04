5dfc3c2457012b34d47abb8a55c111560bb46c13
Saiyam Pathak <saiyam@civo.com>
2022-05-04 18:22:16 +0530

    minor fixes

--- kube124/foreground.sh
+++ kube124/foreground.sh
@@ -1,1 +1,1 @@
-curl https://gist.githubusercontent.com/saiyam1814/3232f17c62b7cf080736c0615e2cd988/raw/d54769d5b1d31e4efa697cedfd11ce0d33e4210e/kube124.sh | sh 
+curl https://gist.githubusercontent.com/saiyam1814/3232f17c62b7cf080736c0615e2cd988/raw/5e23b9dfc0311bbd20cd1c9407ad3a805b89f1b0/kube124.sh | sh
