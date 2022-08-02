7cae0e3672f0b3f586e81fe2dfbec7323048d2d6
Saiyam Pathak <saiyam911@gmail.com>
2022-08-02 23:04:26 +0530

    minikube

---
+++ minikube/foreground.sh
@@ -0,0 +1,8 @@
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt update -y
+sudo apt -y install vim git curl wget kubectl=1.24.3-0
+minikube start --extra-config=kubeadm.ignore-preflight-errors=NumCPU --force --cpus=1
+
---
+++ minikube/step1.md
@@ -0,0 +1,3 @@
+This is a minikube Kubernetes cluster. Minikube makes local Kubernetes development easy and I thought bringing it to killercoda would be cool. 
+
+Once installed, try  `kubectl get nodes`{{execute}} and enjoy a single node minikube cluster ;)  
