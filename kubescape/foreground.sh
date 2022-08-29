09265c602fd36a0dbdc1abb888d02d864ff0fd4d
Suhas Gumma <43647369+suhasgumma@users.noreply.github.com>
2022-08-29 14:30:26 +0530

    Update foreground.sh

--- kubescape/foreground.sh
+++ kubescape/foreground.sh
@@ -1,8 +1,8 @@
-apt install jq
-curl -sfL https://get.k3s.io | sh -
-VERSION=$(curl -L --silent "https://api.github.com/repos/armosec/kubescape/releases/latest" | jq -r .tag_name)
-wget https://github.com/armosec/kubescape/releases/download/$VERSION/kubescape-ubuntu-latest
-chmod +x kubescape-ubuntu-latest 
-mv kubescape-ubuntu-latest kubescape
-mv kubescape /usr/local/bin/
+apt install jq -y &&
+curl -sfL https://get.k3s.io | sh - &&
+VERSION=$(curl -L --silent "https://api.github.com/repos/armosec/kubescape/releases/latest" | jq -r .tag_name) &&
+wget https://github.com/armosec/kubescape/releases/download/$VERSION/kubescape-ubuntu-latest &&
+chmod +x kubescape-ubuntu-latest  &&
+mv kubescape-ubuntu-latest kubescape &&
+mv kubescape /usr/local/bin/ &&
 export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
