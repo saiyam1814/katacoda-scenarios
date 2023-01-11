e43af94fc3c1e56e0f6ccd5f86825050747e4bf9
Saiyam Pathak <saiyam911@gmail.com>
2023-01-11 13:57:45 +0530

    fix 1.25

--- kube125/foreground.sh
+++ kube125/foreground.sh
@@ -1,1 +1,2 @@
-curl https://gist.githubusercontent.com/saiyam1814/c25c100c93c8b3f38c5e3b8bc75b531b/raw/b5836756e488453a8727fa390ca6eaf2b732e67b/kube1.25.sh | sh
+
+curl https://gist.githubusercontent.com/saiyam1814/c25c100c93c8b3f38c5e3b8bc75b531b/raw/cf7395dfec3a85681c57a2cb84e91655b9c3f8d1/kube1.25.sh | sh
