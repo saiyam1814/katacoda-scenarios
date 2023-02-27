96a67ef55ba2dc2544561c0ff8cfb0ef4544043b
Saiyam Pathak <saiyam@civo.com>
2023-02-27 12:20:58 +0530

    update to fix key issue

--- kube126/foreground.sh
+++ kube126/foreground.sh
@@ -1,1 +1,1 @@
-curl https://gist.githubusercontent.com/saiyam1814/03700751b8d1068c18c942cf768ea26f/raw/b65cfe306a7bdbf440fcc95335fea326c21c3415/gistfile1.txt | sh
+curl https://gist.githubusercontent.com/saiyam1814/03700751b8d1068c18c942cf768ea26f/raw/9e06c91f4b8172bfe9c47580910b4b686189df0a/gistfile1.txt | sh
