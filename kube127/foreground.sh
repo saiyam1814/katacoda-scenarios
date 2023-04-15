1ca59d72e6035c650cd5446a9a1144daeec51d35
Saiyam Pathak <saiyam911@gmail.com>
2023-04-15 22:12:07 +0530

    kubernetes 1.27.1

--- kube127/foreground.sh
+++ kube127/foreground.sh
@@ -1,1 +1,1 @@
-curl https://gist.githubusercontent.com/saiyam1814/c3e91322441fdb53bbf5958b943a41f3/raw/8f0b64d3c19d512bfa29d5b834ff0f47b8580d01/kubernetes1.27.sh | sh
+curl https://gist.githubusercontent.com/saiyam1814/c3e91322441fdb53bbf5958b943a41f3/raw/6be692bad0c4c9f3541db102069e110a7e54086a/kubernetes1.27.sh | sh
