7c9160e668d614f9471beab27e59fedd99bfd472
Saiyam Pathak <saiyam@civo.com>
2023-08-18 22:33:07 +0530

    Update foreground.sh

--- kube128/foreground.sh
+++ kube128/foreground.sh
@@ -1,1 +1,1 @@
-curl https://gist.githubusercontent.com/saiyam1814/d87598cf55c71953e288cd22858c0593/raw/f216107023d78778c9ed4811cf131e2ff084bddc/kubernetes1.28.sh | sh
+curl https://gist.githubusercontent.com/saiyam1814/d87598cf55c71953e288cd22858c0593/raw/729db963542356197525ad78e32554a1b885b906/kubernetes1.28.sh | sh
