7371a61892b2e5dac0a76f5b84766a785fc7227d
Saiyam Pathak <saiyam911@gmail.com>
2024-01-27 10:54:45 +0530

    nix fix

--- nix/foreground.sh
+++ nix/foreground.sh
@@ -1,1 +1,1 @@
-curl https://gist.githubusercontent.com/saiyam1814/28cee97b7afbe53b1a7bf64c9ed935aa/raw/dced24280946d68f5ed46ca0b25b945fdeb7eca4/nix.sh | sh
+curl https://gist.githubusercontent.com/saiyam1814/28cee97b7afbe53b1a7bf64c9ed935aa/raw/3d710940ca975ecbc989224931c3335327396805/nix.sh | sh
