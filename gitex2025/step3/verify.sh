850cd5309162934e8b7ea91d076dd7c7c615282a
Saiyam Pathak <saiyam911@gmail.com>
2025-04-23 07:00:20 +0800

    workshop

--- gitex2025/step3/verify.sh
+++ gitex2025/step3/verify.sh
@@ -3,9 +3,9 @@
 ```
 #!/bin/bash
 # Verify ArgoCD server Service is NodePort on 30443
-nodeport=$(kubectl -n argocd get svc argocd-server -o jsonpath='{.spec.ports[?(@.name=="https")].nodePort}')
-type=$(kubectl -n argocd get svc argocd-server -o jsonpath='{.spec.type}')
-if [[ "$type" == "NodePort" && "$nodeport" == "30443" ]]; then
+nodeport=$(kubectl get svc argocd-server -o jsonpath='{.spec.ports[?(@.name=="https")].nodePort}')
+type=$(kubectl  get svc argocd-server -o jsonpath='{.spec.type}')
+if [[ "$type" == "NodePort" && "$nodeport" == "30164" ]]; then
   exit 0
 else
   exit 1
