f89db32ec3ac491b81f893800b8b244d32fb4033
Saiyam Pathak <saiyam911@gmail.com>
2025-08-28 11:42:47 +0530

    kubernetes 1.34

--- kube134/foreground.sh
+++ kube134/foreground.sh
@@ -1,1 +1,1 @@
-curl  https://gist.githubusercontent.com/saiyam1814/b2d21733e6bb2b75b2f9df7433ed68c0/raw/9fda663d022499fa9834dbce0204a2b75acddb7d/kube134.sh | sh
+curl  https://gist.githubusercontent.com/saiyam1814/b2d21733e6bb2b75b2f9df7433ed68c0/raw/7cd652f31154146ca59aec5e340c16612f420af5/kube134.sh | bash
